b5ec8b88f996b60b54a4b19dfbaf89725f65f1fe
Yana <yanochkalaz0560@gmail.com>
2023-09-09 18:20:25 +0300

    Adding validation

--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -1,13 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const CustomInput = ({ label = 'DAY', placeholder = 'DD' }) => {
+const CustomInput = ({ label = 'DAY', placeholder = 'DD', setData }) => {
+    const [value, setValue] = useState('');
+    const [mistake, setMistake] = useState(false)
+    const req = /[0-9]/;
+
+    const handleValidation = (val) => {
+        if (val === '') {
+            setMistake(false);
+            return;
+        }
+        switch (label) {
+            case 'DAY':
+                if (!(val.length === 2 && val[0].match(req) && val[1].match(req) && val < 32 && val > 0)) {
+                    setMistake('Must be a valid day')
+                    return;
+                }
+                setMistake(false)
+                setData(prev => ({...prev, day: val}));
+                break;
+            case 'MONTH':
+                if (!(val.length === 2 && val[0].match(req) && val[1].match(req) && val < 13 && val > 0)) {
+                    setMistake('Must be a valid month')
+                    return;
+                }
+                setMistake(false)
+                setData(prev => ({...prev, month: val}))
+                break;
+            case 'YEAR':
+                if (!(val.length === 4)) {
+                    setMistake('Must be a valid year')
+                    return;
+                }
+                const now = new Date();
+                let y = now.getFullYear();
+                if (!(y + 1 > val)) {
+                    setMistake('Must be a valid year')
+                    return;
+                }
+                setMistake(false)
+                setData(prev => ({...prev, year: val}))
+                break;
+        }
+
+    }
+
+    useEffect(() => {
+        handleValidation(value);
+    }, [value]);
+
     return (
-        <div className='input-block'>
-            <label for='input'>
+        <div className={mistake ? 'input-block mistakes' : 'input-block'}>
+            <label htmlFor='input'>
                 {label}
                 <br></br>
             </label>
-            <input id='input' placeholder={placeholder} type='text' />
+            <input value={value} onChange={e => setValue(e.target.value)} id='input' placeholder={placeholder} type='text' />
+            {mistake &&
+                <div className='error-mess'>{mistake}</div>
+            }
         </div>
     )
 }
--- src/components/InputFields.js
+++ src/components/InputFields.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CustomInput from './CustomInput'
+import arrowImg from '../assets/images/icon-arrow.svg';
 
 const InputFields = () => {
+
+  const [data, setData] = useState({
+    day: '',
+    month: '',
+    year: ''
+  })
+
+  function checkDate(str) {
+    str = str.split('.').reverse();
+    let cur = new Date();
+    let date = new Date(str);
+    cur.setHours(0,0,0,0); 
+    return date !== null && date.getTime() < cur.getTime();
+  }
+
+  const handleSubmit = () => {
+    console.log(checkDate(`${data.day}.${data.month}.${data.year}`))
+  }
+
   return (
-    <form className='input-fields'>
-      <CustomInput placeholder='DD' label='DAY'/>
-      <CustomInput placeholder='MM' label='MONTH'/>
-      <CustomInput placeholder='YYYY' label='YEAR'/>
-    </form>
+    <div >
+      <div className='input-fields'>
+        <CustomInput setData={setData} placeholder='DD' label='DAY' />
+        <CustomInput setData={setData} placeholder='MM' label='MONTH' />
+        <CustomInput setData={setData} placeholder='YYYY' label='YEAR' />
+      </div>
+      <div onClick={handleSubmit} className='input-btn'>
+        <div className='grey-line'></div>
+        <div className='submit-btn'>
+          <img src={arrowImg} alt='Arrow' />
+        </div>
+      </div>
+    </div>
   )
 }
 
